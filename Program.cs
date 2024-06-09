a28804654c5ebaabeef1d737576d1e910e886d52
Zeke Taves <ezekielprof@gmail.com>
2024-06-09 18:58:20 -0500

    Added command line arguments to program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 ﻿using SnekGame.Models;
 using SnekGame.Services;
 
-var drawer = new MapDrawer();
-var handler = new MapHandler(20, 20, 1, drawer);
-await handler.GameLoop();
+var argParser = new ArgumentParser();
+if (argParser.TryParseArgs(args))
+{
+	var drawer = new MapDrawer();
+	var handler = new MapHandler(argParser.MapWidth, argParser.MapHeight, argParser.Foods, drawer);
+	await handler.GameLoop();
+}
+else
+{
+	Console.WriteLine("-width <number> for width\n-height <number> for height\n-foods <number> for number of foods");
+}
